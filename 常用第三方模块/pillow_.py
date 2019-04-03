9db60faa24c76655664540051cd413fc8dfc6509
veronica001 <1491155034@qq.com>
2019-04-03 18:06:09 +0800

    add

---
+++ 常用第三方模块/pillow_.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+基本上，所有的第三方模块都会在PyPI - the Python Package Index上注册，只要找到对应的模块名字，即可用pip安装。
+此外，在安装第三方模块一节中，我们强烈推荐安装Anaconda，安装后，数十个常用的第三方模块就已经就绪，不用pip手动安装。
+'''
